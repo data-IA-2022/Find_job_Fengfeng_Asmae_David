1ddf6a659b84dff7bfb8ffe13714a8a85ac7d146
Dakoro <david.korobetski@gmail.com>
2023-01-30 21:55:17 +0100

    Maj

--- web_app/Find_job_app.py
+++ web_app/Find_job_app.py
@@ -1,54 +1,43 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+from IPython.display import display
 from sklearn.feature_extraction.text import CountVectorizer
 
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
+@st.cache
+def load_data(data):
+    df = data.copy()
+    df = df.drop('Unnamed: 0', axis=1)
+    cat_cols = df.select_dtypes([object]).columns
+    num_cols = df.select_dtypes(exclude="object").columns
+    return df, cat_cols, num_cols
 
-data = pd.read_csv('../clean_data.csv')
-df = data.copy()
-df = df.drop('Unnamed: 0', axis=1)
-st.title('Trouve ton job dans la data science')
-st.dataframe(df)
-fig = make_subplots(rows=2, cols=1)
+data = pd.read_csv('../clean_data.csv', sep=',')
+df, cat_cols, num_cols = load_data(data)
+print(cat_cols)
+print(num_cols)
+# Title
 
-fig.append_trace(go.Bar(
-    x=(df["Nom de la société"]).astype(str),
-    y=(df["salaire_maximum"]).astype(int), name="salaire_maximum"
-), row=1, col=1)
-
-fig.append_trace(go.Bar(
-    x=(df["Nom de la société"]).astype(str),
-    y=(df["salaire_minimum"]).astype(int), name="salaire_minimum"
-), row=2, col=1)
-fig.update_layout(height=800, width=1000, title_text="Distribution des salaires selon l'entreprise", showlegend=True)
-st.plotly_chart(fig)
-
-cv = CountVectorizer()
-comp_tf = cv.fit_transform(df['competences']).toarray()
-comp_tf = pd.DataFrame(data = comp_tf, columns = cv.get_feature_names_out()) # permet d'obtenir un df avec les bons noms de colonne
-names_comp = comp_tf.columns
-comp_tf['Intitulé du poste'] = df['Intitulé du poste']
-postes = df['Intitulé du poste'].unique() # donne la liste des valeurs possibles
-df_gb = comp_tf.groupby('Intitulé du poste')[names_comp].sum()
-
-# caculate the occurrence of competences
-occurrence_comp = comp_tf.sum(axis=0)
-
-df_occ_comp = pd.DataFrame(occurrence_comp)
-df_occ_comp["competences"] = df_occ_comp.index
-df_occ_comp.rename(columns={0 : "occurrence"}, inplace = True)
-
-#df_occ_comp.drop(['Intitulé du poste'], axis=0, inplace=True)
-
-fig = px.bar(df_occ_comp, x="competences", y='occurrence', title="Occurence des compétences")
-st.plotly_chart(fig)
+st.title('Trouve ton job')
 
 
-# neighborhood = st.radio("competences", names_comp)
-fig = px.scatter(df_gb, title="Répartition des compétences pour les différents Intitulés du poste")
-fig.update_traces(marker={'size': 15})
-st.plotly_chart(fig)
+# Sidebar
+st.sidebar.title('Settings')
+check_box = st.sidebar.checkbox(label='Display dataset')
+if check_box:
+    st.dataframe(df)
+    
+select_chart = st.sidebar.selectbox(label="Select the chart type", options=['Histogram'])
 
+if select_chart == 'Histogram':
+    st.sidebar.subheader("Histogram setttings")
+    try:
+        x_values = st.sidebar.selectbox("X axis", options=['Intitulé du poste', 'lieu', 'Nom de la société'])
+        y_values = st.sidebar.selectbox("Y axis", options=num_cols)
+        plot = px.bar(df, x_values, y_values)
+        st.plotly_chart(plot)
+    except Exception as e:
+        print(e)
